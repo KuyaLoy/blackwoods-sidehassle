ea8f1eef513275258d834d7259dd2f5d4c620cfa
Robin Tapiru <robintapiru0894@gmail.com>
2023-07-25 15:54:58 +0400

    offer container odne

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,23 +14,27 @@ window.onscroll = () => {
 };
 
 //services-wrapper
-let oldHeight;
+let oldHeight1;
 $(document).ready(function () {
   let servicesWrapper = $(".services-wrapper");
   let servicesWrapperHeight = servicesWrapper.height();
-  oldHeight = servicesWrapperHeight;
+  oldHeight1 = servicesWrapperHeight;
 
   if ($(window).width() < 699.98) {
     servicesWrapper = servicesWrapper.height(
-      (46 / 100) * servicesWrapperHeight
+      (62.5 / 100) * servicesWrapperHeight
     );
   } else if ($(window).width() < 899.98) {
     servicesWrapper = servicesWrapper.height(
-      (40 / 100) * servicesWrapperHeight
+      (55 / 100) * servicesWrapperHeight
+    );
+  } else if ($(window).width() < 1139.98) {
+    servicesWrapper = servicesWrapper.height(
+      (60 / 100) * servicesWrapperHeight
     );
   } else {
     servicesWrapper = servicesWrapper.height(
-      (55 / 100) * servicesWrapperHeight
+      (75 / 100) * servicesWrapperHeight
     );
   }
 });
@@ -38,5 +42,29 @@ $(document).ready(function () {
 $("#showMoreBtn").click(function () {
   $(this).hide();
   $(".services-wrapper").addClass("show");
-  $(".services-wrapper").height(oldHeight);
+  $(".services-wrapper").height(oldHeight1);
+});
+
+//services-wrapper
+let oldHeight2;
+$(document).ready(function () {
+  let offerWrapper = $(".offer-wrapper");
+  let offerWrapperHeight = offerWrapper.height();
+  oldHeight2 = offerWrapperHeight;
+
+  if ($(window).width() < 699.98) {
+    offerWrapper = offerWrapper.height((57.7 / 100) * offerWrapperHeight);
+  } else if ($(window).width() < 899.98) {
+    offerWrapper = offerWrapper.height((50 / 100) * offerWrapperHeight);
+  } else if ($(window).width() < 1139.98) {
+    offerWrapper = offerWrapper.height((55 / 100) * offerWrapperHeight);
+  } else {
+    offerWrapper = offerWrapper.height((78 / 100) * offerWrapperHeight);
+  }
+});
+
+$("#showMoreBtn2").click(function () {
+  $(this).hide();
+  $(".offer-wrapper").addClass("show");
+  $(".offer-wrapper").height(oldHeight2);
 });
