8136bb0bf1562ba27f4f6ecce2270bbc8866056a
Robin Tapiru <robintapiru0894@gmail.com>
2023-07-25 11:24:59 +0400

    offer unfinished

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,30 @@ window.onscroll = () => {
   }
 };
 
+//services-wrapper
+let oldHeight;
+$(document).ready(function () {
+  let servicesWrapper = $(".services-wrapper");
+  let servicesWrapperHeight = servicesWrapper.height();
+  oldHeight = servicesWrapperHeight;
+
+  if ($(window).width() < 699.98) {
+    servicesWrapper = servicesWrapper.height(
+      (46 / 100) * servicesWrapperHeight
+    );
+  } else if ($(window).width() < 899.98) {
+    servicesWrapper = servicesWrapper.height(
+      (40 / 100) * servicesWrapperHeight
+    );
+  } else {
+    servicesWrapper = servicesWrapper.height(
+      (55 / 100) * servicesWrapperHeight
+    );
+  }
+});
+
 $("#showMoreBtn").click(function () {
-  $(".services-wrapper").toggleClass("show");
+  $(this).hide();
+  $(".services-wrapper").addClass("show");
+  $(".services-wrapper").height(oldHeight);
 });
